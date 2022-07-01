fdbba930ac340db92d47d77d825f3eea79f14db4
ABDUL WASI <73805930+abdwasidev@users.noreply.github.com>
2022-07-01 21:57:09 +0700

    Add files via upload

---
+++ loop_for.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+main(){
+	int i;
+	
+	for(i=1;i<=5;i++){
+		printf("Perulangan ke-%d\n", i);
+	}
+	
+	printf("\n");
+	
+	for(i=5;i>=1;i--){
+		printf("Perulangan ke-%d\n", i);
+	}
+	
+	printf("\n");
+}
